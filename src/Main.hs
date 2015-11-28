0f39c85eb0ef13096ed4bb15b6ac8e15526778d0
karsten gebbert <k@ioctl.it>
2015-11-28 11:27:54 +0100

    WinAmp!

--- src/Main.hs
+++ src/Main.hs
@@ -10,16 +10,17 @@ thing pr1 pr2 pr3 pr4 op1 op2 op3 op4 t fac = do
   (x :: Int) <- Window.pointer_x
   (y :: Int) <- Window.pointer_y
 
-  let t1 = t + ((fromIntegral x) * 0.001)
-  let t2 = t + ((fromIntegral y) * 0.001)
+  let ti = t * 0.1
+  let t1 = t + fromIntegral x * 0.001
+  let t2 = t + fromIntegral y * 0.001
             
   clearColor $= Color4 (0.0 :: CFloat) 0.0 0.0 0.1
-  _ <- color (Color4 (pr1 (realToFrac t) :: CFloat) (pr2 (realToFrac t)) (pr3 (realToFrac t)) (pr4 (realToFrac t) * 0.1))
+  _ <- color (Color4 (pr1 (realToFrac ti) :: CFloat) (pr2 (realToFrac ti)) (pr3 (realToFrac ti)) (pr4 (realToFrac ti) * 0.01))
 
-  let x1 = (pr1 (realToFrac t)  * (op1 fac fac))  :: CFloat
-  let y1 = (pr2 (realToFrac t1) / (op2 fac fac))  :: CFloat
-  let x2 = (pr3 (realToFrac t2) * (op3 fac fac)) :: CFloat
-  let y2 = (pr4 (realToFrac t)  / (op4 fac fac)) :: CFloat
+  let x1 = (pr1 (realToFrac ti)  * op1 fac fac)  :: CFloat
+  let y1 = (pr2 (realToFrac t1) / op2 fac fac)  :: CFloat
+  let x2 = (pr3 (realToFrac t2) * op3 fac fac) :: CFloat
+  let y2 = (pr4 (realToFrac ti)  / op4 fac fac) :: CFloat
            
   renderPrimitive Polygon $ do
       vertex (Vertex2 x1 y1)
